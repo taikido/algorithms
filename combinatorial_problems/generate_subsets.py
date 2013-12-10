e1339a31131c534ffe76c96e91f7ebb384604e22
Teyvonia Thomas <teyvonia@gmail.com>
2013-12-10 07:56:04 -0500

    modified generate subsets algorithm to use tuple representation for subsets (vs strings)

--- combinatorial_problems/generate_subsets.py
+++ combinatorial_problems/generate_subsets.py
@@ -22,22 +22,64 @@ Tree of recursive calls for sub("", "abc")
                    
                    
 """
+
+
+""" Generates subsets of a given set. Uses strings to represent sets/subsets.
+
+    @param so_far: subset element is currently being added to.
+    @param rest: set element is taken from.
+    @param subsets: list of subsets formed.
+"""
 def gen_subsets(so_far, rest, subsets):
     if len(rest) == 0:
         subsets.append(so_far)
-        print so_far
+        #print so_far
     else:
         gen_subsets(so_far+rest[0], rest[1:], subsets) #add to subset, remove from rest, recur
         gen_subsets(so_far, rest[1:], subsets) #don't add to subset, remove from rest, recur
         
         
+""" Generates subsets of a given set. Uses tuple to represent sets/subsets.
+        *Use tuple instead of list (list is mutable, will produce unfavorable results)
+
+    @param so_far: subset element is currently being added to.
+    @param rest: set element is taken from.
+    @param subsets: list of subsets formed.
+"""
+def sub(so_far, rest, subsets):
+    if len(rest) == 0:
+        subsets.append(so_far)
+        
+    else:
+        sub(so_far + tuple(rest[0]), rest[1:], subsets)        
+        sub(so_far, rest[1:], subsets)
+        
+
+              
 def main():
+    
+    so_far = ()
+    rest = ("a", "b", "c")
+    subsets = []
+    
+    sub(so_far, rest, subsets)
+    print "Subsets generated using Tuple Representation are: \n", subsets
+    print
+    
+
     so_far = ""
-    rest = "abc"
+    rest = "1234"
     subsets = []
     gen_subsets(so_far, rest, subsets)
+     
+    print "Subsets generated using String Representation are: \n", subsets    
+    print
     
-    print "Subsets generated are: ", subsets
+    #test tuple merging
+    t = ("a", "b", "c")
+    d = "d"    
+    t += tuple(d)    
+    print t
     
     
 if __name__ == "__main__":
